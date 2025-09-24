beb7654c37fbe148b3a74bd213d461caaa42791e
kutaydolar <168274135+kutaydolar@users.noreply.github.com>
2025-09-24 15:59:03 +0300

    Update Supabase connection settings

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://zkkbandockklkohihplp.supabase.co'
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpra2JhbmRvY2trbGtvaGlocGxwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg3MDYwMTYsImV4cCI6MjA3NDI4MjAxNn0.bFCFKAOw7YKq7PPouLKBs_fe6bZyGARthYFp4xaQzWA'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
