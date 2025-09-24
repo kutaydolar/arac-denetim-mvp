277a8ba1c5a8cb036c1f2b86c5c64b0c71e12f92
kutaydolar <168274135+kutaydolar@users.noreply.github.com>
2025-09-24 10:28:24 +0300

    Fix Serwist configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,3 +4,8 @@ import withSerwist from "@serwist/next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
+
+export default withSerwist({
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+})(nextConfig);
