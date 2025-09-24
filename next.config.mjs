8c778d09bac24492920032c3626f4d0aa58e67ec
kutaydolar <168274135+kutaydolar@users.noreply.github.com>
2025-09-24 10:43:39 +0300

    Remove conflicting webpack configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,6 @@ import withSerwist from "@serwist/next";
 
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    // Ensure CSS files are handled correctly
-    config.module.rules.push({
-      test: /\.css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'postcss-loader'
-      ]
-    });
-    
-    return config;
-  }
 };
 
 export default withSerwist({
