67adc4ac7f69f51a9aec511ba40e14cfa34737c3
kutaydolar <168274135+kutaydolar@users.noreply.github.com>
2025-09-24 10:35:27 +0300

    Fix ESLint version conflict and rename Next.js config

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+@@ .. @@
+-import type { NextConfig } from "next";
+ import withSerwist from "@serwist/next";
+ 
+-const nextConfig: NextConfig = {
++const nextConfig = {
+   reactStrictMode: true,
+ };
